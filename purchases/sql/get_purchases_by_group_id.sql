45a0fd4352fd04c5b9744adc160e09ca566f1bab
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-06-09 14:38:06 -0400

    Add docstring to sql

--- purchases/sql/get_purchases_by_group_id.sql
+++ purchases/sql/get_purchases_by_group_id.sql
@@ -1,1 +1,8 @@
+/*
+Name: get_purchases_by_group_id.sql
+Description: Get all purchases for a group
+Usage: [group_id]
+Return: {id, name, category, group_id, total_cost, purchaser}
+*/
+
 SELECT * FROM purchase WHERE group_id = %(group_id)s
