18ebd01c064c01fa28135dcf2b1b2d8ef4b734c5
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-06-09 14:33:39 -0400

    Update endpoint for purchase categories to be more sql-based

--- analytics/api.py
+++ analytics/api.py
@@ -1,8 +1,4 @@
 from ninja import Router
-from django.http import JsonResponse
-from multiset.db_utils import execute_query
-from pathlib import Path
-from purchases.models import Purchase
 from analytics.services import get_purchase_category_count
 
 router = Router()
--- analytics/services.py
+++ analytics/services.py
@@ -1,16 +1,11 @@
 from multiset.db_utils import execute_query
-from purchases.models import Purchase
-from purchases.services import get_purchases_by_group_id
-from collections import defaultdict
+from pathlib import Path
 
 
 def get_purchase_category_count(group_id: int):
-    rows = get_purchases_by_group_id(group_id)
-    categories = defaultdict(int)
-    for row in rows:
-        category = row["category"]
-        if not category:
-            continue
-        categories[category] += 1
-
-    return categories
+    rows = execute_query(
+        Path("analytics/sql/get_purchase_category_count.sql"),
+        {"group_id": group_id},
+        fetchall=True,
+    )
+    return rows
