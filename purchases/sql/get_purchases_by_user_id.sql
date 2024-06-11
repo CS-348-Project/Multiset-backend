15bb69c2503b546dfc699fbd6a1a32224d6f6cd2
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-06-11 16:20:09 -0400

    Get user name in sql query

--- purchases/sql/get_purchases_by_user_id.sql
+++ purchases/sql/get_purchases_by_user_id.sql
@@ -1,3 +1,4 @@
-SELECT *
-FROM purchase
-where purchaser = %(user_id)s
+SELECT p.id, name, category, total_cost, purchaser, first_name as purchaser_first_name, last_name as purchaser_last_name
+FROM purchase p
+JOIN multiset_user u ON p.purchaser = u.id
+WHERE purchaser = $(user_id)s
