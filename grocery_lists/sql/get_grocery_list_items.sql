507078cf8534f73624e2459ff15432690ba44d35
Emma Huang <emmahuang.ty@gmail.com>
2024-06-11 18:23:54 -0400

    Add member info to grocery list items

--- grocery_lists/sql/get_grocery_list_items.sql
+++ grocery_lists/sql/get_grocery_list_items.sql
@@ -3,5 +3,16 @@ Name: get_grocery_list_items.sql
 Description: Get grocery list items by grocery list id
 Usage: {grocery_list_id}
 */
-SELECT * FROM grocery_list_item
+SELECT 
+  grocery_list_item.id, 
+  grocery_list_item.item_name,
+  grocery_list_item.quantity,
+  grocery_list_item.completed,
+  grocery_list_item.notes,
+  grocery_list_item.member_id,
+  multiset_user.first_name AS member_first_name,
+  multiset_user.last_name AS member_last_name
+FROM grocery_list_item
+JOIN member ON member.id = grocery_list_item.member_id
+JOIN multiset_user ON member.user_id = multiset_user.id
 WHERE grocery_list_id = %(grocery_list_id)s
