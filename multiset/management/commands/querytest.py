4da5437dad3264cf67f4192f5e414e5649266738
Weldon Scott <wascott@uwaterloo.ca>
2024-06-12 22:08:21 -0400

    Adding expected/actual prints for fails

--- multiset/management/commands/querytest.py
+++ multiset/management/commands/querytest.py
@@ -8,32 +8,38 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         passes = 0
-        fails = 0
-        errors = 0
+        fails = []
+        errors = []
 
         # get all the sql files in the tests directory
         files = glob(self.TEST_PATH + "/**/*.sql", recursive=True)
 
-        for file in files:
-            raw_result = execute_query(file, fetchall=True)
-            string_result = self._to_csv(raw_result)
+        print(f"Running {len(files)} tests...")
 
-            # now, let's compare with the expected output
+        for file in files:
+            # let's get the expected output
             with open(file, "r") as f:
                 contents = f.read()
 
             content_lines = contents.split("\n")
 
-            # get the expected output
+            # get the expected output or return an error if it doesn't exist
             try:
                 start = content_lines.index("begin expected") + 1
                 end = content_lines.index("end expected") - 1
+
+                if start >= end:
+                    raise ValueError("Invalid expected output range")
             except ValueError:
                 print("E", end="")
-                errors += 1
+                errors.append(file)
                 continue
 
             expected_output = "\n".join(content_lines[start : end + 1])
+
+            # now, we get the result of the query
+            raw_result = execute_query(file, fetchall=True)
+            string_result = self._to_csv(raw_result)
 
             if string_result == expected_output:
                 print(".", end="")
@@ -41,10 +47,26 @@ class Command(BaseCommand):
 
             else:
                 print("F", end="")
-                fails += 1
+                fails.append(
+                    {"file": file, "result": string_result, "expected": expected_output}
+                )
 
         print()
-        print(f"Passes: {passes}, Fails: {fails}, Errors: {errors}")
+        print(f"Passes: {passes}, Fails: {len(fails)}, Errors: {len(errors)}")
+
+        if len(fails) > 0:
+            print("Fails:")
+            for fail in fails:
+                print(f"File: {fail['file']}")
+                print("Expected:")
+                print(fail["expected"])
+                print("Result:")
+                print(fail["result"])
+
+        if len(errors) > 0:
+            print("Errors:")
+            for error in errors:
+                print(f"{error} (no expected output found in SQL file)")
 
     def _to_csv(self, result: list) -> str:
         # convert the result to a csv string
