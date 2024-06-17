f30798a14ba8dafcd76d24947e68f50d0433c557
Emma Huang <emmahuang.ty@gmail.com>
2024-06-17 08:59:52 -0400

    WIP

--- grocery_lists/sql/get_grocery_list_items.sql
+++ grocery_lists/sql/get_grocery_list_items.sql
@@ -10,6 +10,8 @@ SELECT
     'last_name', multiset_user.last_name
   ) AS member
 FROM grocery_list_item
-JOIN member ON member.id = grocery_list_item.member_id
+JOIN member ON 
+  member.user_id = grocery_list_item.requester_user_id AND 
+  member.group_id = grocery_list_item.requester_group_id
 JOIN multiset_user ON member.user_id = multiset_user.id
 WHERE grocery_list_id = %(grocery_list_id)s
