85542f934314ef149f3968691c38a58808d7d88f
Weldon Scott <wascott@uwaterloo.ca>
2024-06-17 13:18:40 -0400

    Decorator to verify group validity

--- optimization/api.py
+++ optimization/api.py
@@ -1,23 +1,27 @@
 from ninja import Router
 from django.http import JsonResponse
 
+from optimization.utils import _verify_group
 from optimization.services import calculate, flag, toggle
 
 router = Router()
 
 
 @router.get("/flag")
+@_verify_group
 def flag_handler(request, group_id: int):
     ret = flag(group_id)
     return JsonResponse(ret, status=200)
 
 
 @router.patch("/toggle")
+@_verify_group
 def toggle_handler(request, group_id: int):
     ret = toggle(group_id)
     return JsonResponse(ret, status=200)
 
 
 @router.post("/calculate")
+@_verify_group
 def calculate_handler(request, group_id: int):
     return calculate(group_id)
