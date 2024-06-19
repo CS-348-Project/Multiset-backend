4d4da5ecf6016ec8ad1870f8db2e201f53ea73ef
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-06-19 15:15:42 -0400

    Fix SQL queries for purchase

--- purchases/sql/get_purchases_by_group_id.sql
+++ purchases/sql/get_purchases_by_group_id.sql
@@ -5,7 +5,7 @@ Usage: [group_id]
 Return: {id, name, category, group_id, total_cost, purchaser}
 */
 
-SELECT p.id, name, category, total_cost, purchaser, first_name as purchaser_first_name, last_name as purchaser_last_name
+SELECT p.id, name, category, total_cost, purchaser_user_id, first_name as purchaser_first_name, last_name as purchaser_last_name
 FROM purchase p
-JOIN multiset_user u ON p.purchaser = u.id
+JOIN multiset_user u ON p.purchaser_user_id = u.id
 WHERE group_id = %(group_id)s
--- purchases/sql/get_purchases_by_user_id.sql
+++ purchases/sql/get_purchases_by_user_id.sql
@@ -1,4 +1,11 @@
-SELECT p.id, name, category, total_cost, purchaser, first_name as purchaser_first_name, last_name as purchaser_last_name
+/*
+Name: get_purchases_by_user_id.sql
+Description: Get all purchases for a group and user id
+Usage: [user_id]
+Return: {id, name, category, group_id, total_cost, purchaser}
+*/
+
+SELECT p.id, name, category, total_cost, purchaser_user_id, first_name as purchaser_first_name, last_name as purchaser_last_name
 FROM purchase p
-JOIN multiset_user u ON p.purchaser = u.id
-WHERE purchaser = $(user_id)s
+JOIN multiset_user u ON p.purchaser_user_id = u.id
+WHERE purchaser_user_id = %(user_id)s
