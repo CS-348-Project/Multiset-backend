996a75c0de1a9ecc3335b48acad5d12d958971f3
Kenneth Ruan <kennethjruan@gmail.com>
2024-06-19 01:10:22 -0400

    Update user schema

--- users/models.py
+++ users/models.py
@@ -1,12 +1,9 @@
-from datetime import datetime
 from ninja import Schema
-from typing import List, Optional
 
 class User(Schema):
     id: int
-    user_token: str
+    email: str
+    google_id: str
     first_name: str
     last_name: str
-    email: str
-    password: str
 
