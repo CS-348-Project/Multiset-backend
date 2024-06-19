56b2382a0f1698280712606555a53dc011ebfca3
Emma Huang <emmahuang.ty@gmail.com>
2024-06-19 16:38:06 -0400

    Remove unused sql

--- settlements/sql/get_member_info_by_id.sql
+++
@@ -1,7 +0,0 @@
-/*
-Name: get_member_info_by_id.sql
-Description: Get member info (user_id, group_id, first_name, last_name) by id
-Usage: {member_user_id, member_group_id}
-*/
-SELECT %(member_user_id)s as member_user_id, %(member_group_id)s as member_group_id, multiset_user.first_name, multiset_user.last_name FROM multiset_user 
-WHERE multiset_user.id = %(member_user_id)s;
