938300d3ce0308941812ba09fcbbf5bc809ea5ad
Catherine Kim <80084168+catherinek07@users.noreply.github.com>
2024-06-20 12:35:10 -0400

    Delete login/serializers.py

--- login/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class AuthSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    given_name = serializers.CharField(required=False)
-    family_name = serializers.CharField(required=False)
