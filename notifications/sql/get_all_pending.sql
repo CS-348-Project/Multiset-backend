fbb56a8cfaec44c67448d0beda1be25e4cea97b2
Weldon Scott <wascott@uwaterloo.ca>
2024-07-07 22:46:52 -0400

    Notification email scheduling

--- notifications/sql/get_all_pending.sql
+++ notifications/sql/get_all_pending.sql
@@ -4,6 +4,15 @@ Description: Gets all notifications that have not had an email sent for all
              users with email notifications enabled.
 */
 
-SELECT n.* FROM notification n
-JOIN multiset_user u ON n.user_id = u.id
-WHERE u.email_notifications = TRUE AND n.email_sent = FALSE;
+SELECT *, (
+    -- get all notifications for the user
+    SELECT json_agg(n)
+    FROM (
+        SELECT n.*
+        FROM notification n
+        WHERE n.user_id = u.id AND n.email_sent = FALSE
+        ORDER BY n.created_at ASC -- oldest first
+    ) n
+) notifications
+FROM multiset_user u
+WHERE u.email_notifications = TRUE;
