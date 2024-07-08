64dce9ca1f47c34e2056bbec424a72708bfa137d
Emma Huang <emmahuang.ty@gmail.com>
2024-07-08 08:37:36 -0400

    Add ordering

--- member_activity_logs/sql/get_member_activity_logs_by_group_id.sql
+++ member_activity_logs/sql/get_member_activity_logs_by_group_id.sql
@@ -3,4 +3,4 @@ Name: get_member_activity_logs_by_group_id.sql
 Description: retrieves all member activity logs for a group
 Usage: {group_id}
 */
-SELECT * FROM member_activity_logs WHERE member_group_id = %(group_id)s;
+SELECT * FROM member_activity_logs WHERE member_group_id = %(group_id)s ORDER BY created_at DESC;
