6f23ea763d0b067c198dce86a6a0803a7ef3b017
Emma Huang <emmahuang.ty@gmail.com>
2024-07-17 15:53:28 -0400

    Remove purchases

--- purchases/sql/get_purchase_by_id.sql
+++ purchases/sql/get_purchase_by_id.sql
@@ -2,9 +2,9 @@
 Name: get_purchases_by_id.sql
 Description: Get purchase info based on purchase id
 Usage: {purchase_id}
-Return: {id, category, name, total_cost, created_at, purchaser_group_id}
+Return: {id, category, name, total_cost, created_at}
 */
 
-SELECT id, category, name, total_cost, created_at, purchaser_group_id
+SELECT id, category, name, total_cost, created_at
 FROM purchase p
 WHERE id = %(purchase_id)s
