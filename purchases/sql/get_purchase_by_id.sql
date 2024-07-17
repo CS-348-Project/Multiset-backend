cf650fcfcca15ec57082b1d50603316f3b842c81
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-07-17 16:06:21 -0400

    Fix typo

--- purchases/sql/get_purchase_by_id.sql
+++ purchases/sql/get_purchase_by_id.sql
@@ -2,7 +2,7 @@
 Name: get_purchases_by_id.sql
 Description: Get purchase info based on purchase id
 Usage: {purchase_id}
-Return: {id, category, name, total_cost, created_at, purchase_user_id, purchaser_group_id}
+Return: {id, category, name, total_cost, created_at, purchaser_user_id, purchaser_group_id}
 */
 
 SELECT id, category, name, total_cost, created_at, purchaser_user_id, purchaser_group_id
