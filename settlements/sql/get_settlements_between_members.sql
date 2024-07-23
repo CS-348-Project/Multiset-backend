a6f35c4fdedcdae6efe9b7e20a538f0a487d58c0
Emma Huang <emmahuang.ty@gmail.com>
2024-07-23 14:21:55 -0400

    Add back deletion  triggers

--- settlements/sql/get_settlements_between_members.sql
+++ settlements/sql/get_settlements_between_members.sql
@@ -11,7 +11,11 @@ WITH settlements AS (
     sender_user_id = %(member2_user_id)s AND receiver_user_id = %(member1_user_id)s)
 )
 
-SELECT settlements.*, m1.first_name AS sender_first_name, m1.last_name AS sender_last_name, m2.first_name AS receiver_first_name, m2.last_name AS receiver_last_name
+SELECT settlements.*, 
+  m1.first_name AS sender_first_name, 
+  m1.last_name AS sender_last_name, 
+  m2.first_name AS receiver_first_name, 
+  m2.last_name AS receiver_last_name
 FROM settlements
 JOIN multiset_user m1 ON settlements.sender_user_id = m1.id
 JOIN multiset_user m2 ON settlements.receiver_user_id = m2.id;
